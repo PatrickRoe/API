c7620c59bf65d0ac263e6e7ce2c56c5bdcd60a35
Patrick <patrick.roessler@dci-student.org>
2024-07-02 09:57:11 +0200

    api

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get('/api/random', (req, res) => {
-  const randomNumber = Math.floor(Math.random() * 100) + 1; 
+app.get("/api/random", (req, res) => {
+  const randomNumber = Math.floor(Math.random() * 100) + 1;
   res.json({ random: randomNumber });
 });
 
 app.listen(port, () => {
-    console.log(`Server läuft auf http://localhost:${port}/api/random`);
-  });
-  
- 
+  console.log(`Server läuft auf http://localhost:${port}/api/random`);
+});
